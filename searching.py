a1892a25dcbeb12a6077f52cb1b9a62d35227bc1
Agnes Malarzova <278115@vutbr.cz>
2026-04-08 09:17:18 +0200

    ukol2

--- searching.py
+++ searching.py
@@ -20,5 +20,26 @@ def main():
 
 
 
+
+def linear_search(seznam, hledane_cislo):
+    pozice = []
+    pocet = 0
+
+    for i, hodnota in enumerate(seznam):
+        if hodnota == hledane_cislo:
+            pozice.append(i)
+            pocet += 1
+
+    return {
+        "positions" : pozice,
+        "count" : pocet
+    }
+
+def main():
+    seznam = read_data("sequential.json","unordered_numbers")
+    hledane_cislo = 5
+    vysledek = linear_search(seznam, hledane_cislo)
+    print(vysledek)
+
 if __name__ == "__main__":
     main()
