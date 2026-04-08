8e36ad46b042cbcbd481c027baea252d2b65173d
Agnes Malarzova <278115@vutbr.cz>
2026-04-08 10:47:14 +0200

    mereni casu

--- searching.py
+++ searching.py
@@ -65,5 +65,52 @@ def main():
     index = binary_search(seznam_cisel, hledane_cislo)
 
     print(index)
+
+import time
+import matplotlib.pyplot as plt
+from generators import ordered_sequence
+
+
+def measure_time(funkce, *argumenty):
+    start = time.perf_counter()
+    funkce(*argumenty)
+    end= time.perf_counter()
+
+    return end - start
+
+def main():
+    velikosti = [100, 500, 1000, 5000,  10000 ]
+
+    casy_linear=[]
+    casy_binary=[]
+
+    for n in velikosti:
+        seznam_cisel = ordered_sequence(n)
+        hledane_cislo = seznam_cisel[-1]
+
+        cas_linear = measure_time(linear_search, seznam_cisel, hledane_cislo)
+        casy_linear.append(cas_linear)
+
+        cas_binary = measure_time(binary_search, seznam_cisel, hledane_cislo)
+        casy_binary.append(cas_binary)
+
+        print(len(velikosti))
+        print(len(casy_linear))
+        print(len(casy_binary))
+
+        plt.plot(velikosti, casy_linear, label="lineqarni vyhledavani")
+        plt.plot(velikosti, casy_binary, label= "binarni vyhledavani")
+
+        plt.xlabel("Velikost vstupu")
+        plt.ylabel("Čas [s]")
+        plt.title("mereni casu behem algoritmu")
+        plt.legend()
+        plt.show()
+
+
+
+
+
+
 if __name__ == "__main__":
     main()
